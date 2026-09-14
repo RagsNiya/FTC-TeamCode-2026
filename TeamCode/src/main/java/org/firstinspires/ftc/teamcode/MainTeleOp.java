443b284e816f4b428c43c04d370798c63de66253
RagsNiya <rags.niya@gmail.com>
2026-09-14 14:46:19 -0400

    Add MainTeleOp class for robot control

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MainTeleOp.java
@@ -0,0 +1,15 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "MainTeleOP")
+public class MainTeleOp extends LinearOpMode{
+  @Override
+    public void runOpMode() {
+      waitForStart();
+      while (opModeIsActive()){
+        //robot control code goes here
+      }
+    }
+}
